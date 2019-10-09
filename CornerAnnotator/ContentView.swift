e4f175ec65585c681859dfeb49b305de69ab14e7
Magnus Jensen <magnuskahr@icloud.com>
2019-10-09 09:51:51 +0200

    Initial Commit

---
+++ CornerAnnotator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CornerAnnotator
+//
+//  Created by Magnus Jensen on 09/10/2019.
+//  Copyright © 2019 Magnus Jensen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
